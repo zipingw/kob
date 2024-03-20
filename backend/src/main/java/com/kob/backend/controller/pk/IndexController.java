3307ed4fd14c24af35ab983a1508164c9455d396
zipingw <zipingww@163.com>
2024-03-20 11:46:37 +0800

    update backend 完善数据库接入与登录

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -3,10 +3,10 @@ package com.kob.backend.controller.pk;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
-@RequestMapping("/pk/")
 public class IndexController {
-    @RequestMapping("index/")
+    @RequestMapping("/")
     public String index(){
         return "/pk/index.html";
     }
--- backend/src/main/java/com/kob/backend/pojo/User.java
+++ backend/src/main/java/com/kob/backend/pojo/User.java
@@ -11,5 +11,4 @@ public class User {
     private Integer id;
     private String username;
     private String password;
-    private Integer rating;
 }
