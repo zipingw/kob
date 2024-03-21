2ed5b8cc3007eb9e20aa16c2fbb0bbb6b048cc34
zipingw <zipingww@163.com>
2024-03-21 14:26:29 +0800

    完善登录加密

--- backend/src/main/java/com/kob/backend/controller/user/UserController.java
+++ backend/src/main/java/com/kob/backend/controller/user/UserController.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.kob.backend.pojo.User;
 import com.kob.backend.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +35,10 @@ public class UserController {
     public String addUser(@PathVariable int userId,
                           @PathVariable String username,
                           @PathVariable String password) {
-        User user = new User(userId, username, password);
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = new User(userId, username, encodedPassword);
         userMapper.insert(user);
         return "Add User Successfully";
     }
